c8609a02cb771bb8d643f6ed1661e797a33bdd38
Arvid Sigvardsson <arvid.sigvardsson@gmail.com>
2014-04-07 14:16:50 +0200

    Integrated json questions. Seems to work

--- QuizCrush/QCQuestionProvider.h
+++ QuizCrush/QCQuestionProvider.h
@@ -11,6 +11,7 @@
 
 @interface QCQuestionProvider : NSObject
 
+-(id) init;
 -(QCQuestion *) provideQuestionOfCategory:(NSNumber *) category;
 
 @end
--- QuizCrush/QCQuestionProvider.m
+++ QuizCrush/QCQuestionProvider.m
@@ -8,20 +8,92 @@
 
 #import "QCQuestionProvider.h"
 
+@interface QCQuestionProvider ()
+
+@property NSArray *questionsArray;
+
+@end
+
 @implementation QCQuestionProvider
+
+-(id) init {
+    if(!(self = [super init])) {
+        return self;
+    }
+
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"svenskaQuestions" ofType:@"json"];
+    NSData *JSONData = [NSData dataWithContentsOfFile:filePath
+                                              options:NSDataReadingMappedIfSafe
+                                                error:nil];
+    
+    _questionsArray = [NSJSONSerialization JSONObjectWithData:JSONData
+                                                              options:NSJSONReadingAllowFragments
+                                                                error:nil];
+
+    return self;
+}
 
 -(QCQuestion *) provideQuestionOfCategory:(NSNumber *) category{
     // TODO placeholder implementation
     
-    QCQuestion *q = [[QCQuestion alloc] initWithCategory:@5
-                                                    topic:@"Golf"
-                                           questionString:@"Vilket folkslag uppfann en stamform till golf?"
-                                                  answers:@[@"Romarna", @"Grekerna", @"Mayafolket", @"Aztekerna"]
-                                       correctAnswerIndex:@0];
-
+//    QCQuestion *q = [[QCQuestion alloc] initWithCategory:@5
+//                                                    topic:@"Golf"
+//                                           questionString:@"Vilket folkslag uppfann en stamform till golf?"
+//                                                  answers:@[@"Romarna", @"Grekerna", @"Mayafolket", @"Aztekerna"]
+//                                       correctAnswerIndex:@0];
+//
     
+    NSDictionary *d = @{@"cat": @99};
     
+    while (!([d[@"cat"] isEqualToNumber:category])) {
+        int index = arc4random_uniform((u_int32_t)[_questionsArray count]);
+        d = _questionsArray[index];
+    }
+    
+    NSString *correctAnswer = d[@"ac"];
+    
+    NSArray *answers = [self shuffleArray:@[d[@"ac"], d[@"a1"], d[@"a2"], d[@"a3"]]];
+    
+    NSNumber *correctIndex = @([answers indexOfObject:correctAnswer]);
+    
+    QCQuestion *q = [[QCQuestion alloc] initWithCategory:d[@"cat"]
+                                                   topic:d[@"tp"]
+                                          questionString:d[@"t"]
+                                                 answers:answers
+                                      correctAnswerIndex:correctIndex];
+        
     return q;
+//    return nil;
 }
+
+-(NSArray *) shuffleArray:(NSArray *) array {
+    NSMutableArray *returnArray = [NSMutableArray arrayWithArray:array];
+    int n = (int) [array count];
+    int j;
+    for (int i = n - 1; i > 0; i--) {
+        j = arc4random_uniform(i + 1);
+        [returnArray exchangeObjectAtIndex:j withObjectAtIndex:i];
+    }
+    return returnArray;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
